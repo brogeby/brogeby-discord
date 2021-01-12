d8e9ab11a60207422d08f2d3de9b1542cd0b7625
brogeby <brogeby@gmail.com>
2021-01-12 13:46:08 +0100

    New command, announcement with embed

---
+++ commands/moderators/announcement.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  commands: ['announcement', 'announce'],
+  description: 'Returns an embed with the desired information',
+  callback: (message, arguments, text) => {
+    if (arguments.length > 0) {
+      let content = arguments.join(' ')
+      const embed = new MessageEmbed()
+        .setTitle('ANNOUNCEMENT')
+        .setColor(0xff0000)
+        .setDescription(content)
+      message.delete()
+      message.channel.send(embed)
+    } else {
+      message.channel.send('You have to type something, ie: "!announcement Exampletext"')
+    }
+  },
+}
