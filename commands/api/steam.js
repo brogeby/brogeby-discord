d69ed8381a849282b1069b63e0fd15e4182e6fad
brogeby <brogeby@gmail.com>
2021-01-13 13:30:01 +0100

    added cat, dog api and get news from steam

---
+++ commands/api/steam.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const axios = require('axios')
+
+module.exports = {
+  commands: 'steam',
+  description: 'Returns the latest news for the desired game from Steam!',
+  minArgs: 1,
+  maxArgs: 1,
+  expectedArgs: '<ID> || Provide the ID for the game which can be found on https://steamdb.info/',
+
+  callback: (message, arguments, text) => {
+    const gameId = arguments[0]
+    axios
+      .get(
+        `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${gameId}&count=3&maxlength=300&format=json`
+      )
+      .then((res) => {
+        const latestNews = res.data.appnews.newsitems[0]
+
+        const embed = new Discord.MessageEmbed()
+          .setTitle(latestNews.title)
+          .setURL(latestNews.url)
+          .addFields({ name: latestNews.feedlabel, value: latestNews.url })
+        message.channel.send(embed)
+      })
+      .catch((err) => console.error(err))
+  },
+}
