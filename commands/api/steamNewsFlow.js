7eaa1fd99ade2f782516284f47e93cacc81f0603
brogeby <brogeby@gmail.com>
2021-01-19 14:07:32 +0100

    newsflow fixes

--- commands/api/steamNewsFlow.js
+++ commands/api/steamNewsFlow.js
@@ -10,21 +10,21 @@ module.exports = {
   expectedArgs: '<ID> || Provide the ID for the game which can be found on https://steamdb.info/',
 
   callback: (message, arguments, text, client) => {
-    const channelId = '796767772256239636'
-    const gameId = arguments[0]
-
     const requestNewsLoop = setInterval(() => {
+      const channelId = '796767772256239636'
+      const gameId = arguments[0]
       let latestNews = ''
 
       axios
         .get(
           `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${gameId}&count=3&maxlength=300&format=json`
         )
+        .then(
+          console.log('API has been successfully fetched, deleting previous messages'),
+          message.channel.bulkDelete(3, true)
+        )
         .then((res) => {
           latestNews = res.data.appnews.newsitems[0]
-        })
-        .then(message.channel.bulkDelete(3, true))
-        .then((messages) => {
           const unixTimeCreated = latestNews.date
           const createdAt = new Date(unixTimeCreated * 1000)
           const embed = new Discord.MessageEmbed()
@@ -35,8 +35,9 @@ module.exports = {
             .addFields({ name: 'Post ID', value: latestNews.gid, inline: true })
             .setFooter(`Game ID: ${latestNews.appid}`)
           message.channel.send(embed)
+          console.log('New message has been sent')
         })
         .catch((err) => console.error(err))
-    }, 1000 * 86400)
+    }, 3000)
   },
 }
