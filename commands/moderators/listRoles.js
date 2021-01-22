e6e3a9a61354b0b1cbafcf448d4262a451ec3816
brogeby <brogeby@gmail.com>
2021-01-22 15:57:05 +0100

    removed an undefined console.log

--- commands/moderators/listRoles.js
+++ commands/moderators/listRoles.js
@@ -6,18 +6,7 @@ module.exports = {
   permission: 'MANAGE_ROLES',
 
   callback: async (message, arguments, text, client) => {
-    // let name = ''
-
-    // let role = ''
-
-    // let roles = message.guild.roles.cache.forEach((role) => {
-    //   name = role.name
-    //   id = role.id
-    //   console.log(role.name, role.id)
-    // })
-
     let rolemap = message.guild.roles.cache.sort((a, b) => b.position - a.position).map((r) => r)
-    console.log(rolemap.id)
     if (rolemap.length > 1024) rolemap = 'To many roles to display'
     if (!rolemap) rolemap = 'No roles'
     const embed = new Discord.MessageEmbed().addField('Roles', rolemap)
